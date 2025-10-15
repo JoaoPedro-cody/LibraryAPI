2e9887f5f1776fd3035276fc631009cf5300be7d
JoaoPedro-cody <codyla2313@gmail.com>
2025-10-15 15:37:52 -0300

    create userservice and usercontroller

---
+++ src/main/java/io/silva/bookshop/controller/UserController.java
@@ -0,0 +1,40 @@
+package io.silva.bookshop.controller;
+
+import io.silva.bookshop.model.User;
+import io.silva.bookshop.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
+public class UserController {
+    UserService userService;
+
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> searchAllUsers(){
+        return userService.searchUsers();
+    }
+
+    @GetMapping("{email}")
+    public User searchUserByEmail(@PathVariable String email){
+        return userService.searchUserEmail(email);
+    }
+
+    @PutMapping("{email}")
+    public void updateUserByEmail(@RequestBody User user, @PathVariable String email){
+        userService.updateUser( user, email);
+    }
+
+    @DeleteMapping("{email}")
+    public void deleteUserByEmail(@PathVariable String email){
+        userService.deleteUser(email);
+    }
+}
