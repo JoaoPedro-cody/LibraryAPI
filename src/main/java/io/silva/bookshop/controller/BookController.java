89bdfc5b581df3a3aa938aec9cf3e48798bdca09
jotacody <jotagit00@gmail.com>
2025-10-16 16:06:26 -0300

    Create BookService and BookController

---
+++ src/main/java/io/silva/bookshop/controller/BookController.java
@@ -0,0 +1,45 @@
+package io.silva.bookshop.controller;
+
+import io.silva.bookshop.model.Book;
+import io.silva.bookshop.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("books")
+@AllArgsConstructor
+public class BookController {
+    private BookService bookService;
+
+    @PostMapping("/save")
+    public void saveBook(@RequestBody Book book){
+        bookService.saveBook(book);
+    }
+
+    @GetMapping("/all-books")
+    public List<Book> searchBook(){
+        return bookService.listAll();
+    }
+
+    @GetMapping("/by-title/{title}")
+    public List<Book> searchBookByTitle(@PathVariable String title){
+        return bookService.listBookByTitle(title);
+    }
+
+    @GetMapping("/by-author/{author}")
+    public List<Book> searchByAuthor(@PathVariable String author){
+        return bookService.listByAuthor(author);
+    }
+
+    @PutMapping("/update/{id}")
+    public void updateBook(@PathVariable String id, @RequestBody Book b){
+        bookService.updateBook(id, b);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void  deleteBook(@PathVariable String id){
+        bookService.deleteBook(id);
+    }
+}
--- src/main/java/io/silva/bookshop/repository/BookRepository.java
+++ src/main/java/io/silva/bookshop/repository/BookRepository.java
@@ -2,8 +2,17 @@ package io.silva.bookshop.repository;
 
 import io.silva.bookshop.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository  extends JpaRepository<Book, UUID> {
+    @Query("""
+        select b from Book b order by b.title asc
+    """)
+    List<Book> findAllBooksSorted();
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
 }
