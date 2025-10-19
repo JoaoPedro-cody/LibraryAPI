66bbc3195be4a14444a81897c84b408095773516
jotacody <jotagit00@gmail.com>
2025-10-19 03:30:03 -0300

    Create notFoundException

--- src/main/java/io/silva/bookshop/repository/LoanRepository.java
+++ src/main/java/io/silva/bookshop/repository/LoanRepository.java
@@ -1,9 +1,14 @@
 package io.silva.bookshop.repository;
 
+import io.silva.bookshop.model.Book;
 import io.silva.bookshop.model.Loan;
+import io.silva.bookshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LoanRepository extends JpaRepository<Loan, UUID> {
+    List<Loan> findByBook(Book b);
+    List<Loan> findByUser(User user);
 }
